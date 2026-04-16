ac166fe838b67a837912d9ce83c784aedfdcd040
harshada123-png <salunkeharshada22@gmail.com>
2026-04-17 01:02:34 +0530

    loops in java

---
+++ IterationStatement/DoWhileExample.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+        
+        int sec =10;
+
+        do{
+            System.out.println("time remaining: " + sec + " seconds");
+            sec--;
+        }while(sec > 0);
+
+    }
---
+++ IterationStatement/WhileLoopExample.java
@@ -0,0 +1,14 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        
+        int sec =10;
+
+        while(sec > 0){
+            System.out.println("time remaining: " + sec + " seconds");
+            sec--;
+        }
+
+        int i
+    }
+    
+}
