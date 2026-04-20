f6180476a3fa830fca51b8dc7e5b7208a765a07f
harshada123-png <salunkeharshada22@gmail.com>
2026-04-20 09:18:51 +0530

    practice patterns

---
+++ Patterns/Left_Tringle.java
@@ -0,0 +1,20 @@
+public class Left_Tringle {
+    public static void main(String[] args) {
+       int n=5;
+       for(int i =1; i<=n; i++){
+
+        // space print 
+        for(int j=1; j<=n-i; j++){
+            System.out.print(" ");
+        }
+
+        // star print
+        for (int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+       }
+
+    }
+    
+}
