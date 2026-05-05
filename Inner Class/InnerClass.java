1f591653612af1b9cb03b5f7991512128efdd784
harshada123-png <salunkeharshada22@gmail.com>
2026-05-05 14:27:40 +0530

    learned Syncronization and Inner Class

---
+++ Inner Class/InnerClass.java
@@ -0,0 +1,16 @@
+class Outer{
+    int x=10;
+    class Inner{
+        void display(){
+            System.out.println("Value of x is: "+x);
+        }
+    }
+}
+
+public class InnerClass {
+    public static void main(String[] args) {
+        Outer o=new Outer();
+        Outer.Inner i=o.new Inner();
+        i.display();
+    }
+}
